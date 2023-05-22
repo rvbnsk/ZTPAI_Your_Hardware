9c228881086b509c43adf62cf579f253551aaff0
rvbnsk <58656716+rvbnsk@users.noreply.github.com>
2023-05-22 22:14:42 +0200

    Added tests

---
+++ src/test/java/com/wdpai/your/hardware/wdpaiyourhardware/WdpaiYourHardwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdpai.your.hardware.wdpaiyourhardware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WdpaiYourHardwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
